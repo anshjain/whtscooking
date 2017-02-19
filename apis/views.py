50630456b5be7e52367a566dfae2be040fe30b3e
anshjain <jain.scs@gmail.com>
2017-02-19 19:17:08 +0530

    Update user and default pages

--- apis/views.py
+++ apis/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
-# Create your views here.
+
+class HomeView(TemplateView):
+    """ home page """
+    template_name = "home.html"
+
+    def get_context_data(self, **kwargs):
+        context_data = super(HomeView, self).get_context_data(**kwargs)
+        context_data['vendors'] = ['anna', 'sahyadri']
+        context_data['timings'] = ['Breakfast', 'Lunch', 'Evening-snacks', 'Dinner']
+        context_data['food_types'] = ['veg', 'non-veg']
+        return context_data
--- whtscooking/urls.py
+++ whtscooking/urls.py
@@ -1,12 +1,14 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 
-from profile import views
+from profile.views import LoginView, logout_view
+from apis.views import HomeView
 
 urlpatterns = [
     # normal views
-    url(r'^login$', views.LoginView.as_view(), name='login'),
-    url(r'^logout$', views.logout_view, name='logout'),
+    url(r'^$', HomeView.as_view(), name='home'),
+    url(r'^login$', LoginView.as_view(), name='login'),
+    url(r'^logout$', logout_view, name='logout'),
 
     # Admin views
     url(r'^admin/', include(admin.site.urls)),
